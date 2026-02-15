cef9e3152dbb4d28625e67a080161a005c4cb715
lagneshmitrareturns <lagneshmitrareturns@gmail.com>
2026-02-15 23:10:54 +0530

    Update firebase.config.js

--- firebase.config.js
+++ firebase.config.js
@@ -1,11 +1,14 @@
+// Firebase core
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.9.0/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "PASTE",
-  authDomain: "PASTE",
-  projectId: "PASTE",
-  appId: "PASTE"
+  apiKey: "AIzaSyDvxM3XH5IOm66i_bZ4PIhwkDGU3Dq5diE",
+  authDomain: "lagneshmitra-e57b8.firebaseapp.com",
+  projectId: "lagneshmitra-e57b8",
+  storageBucket: "lagneshmitra-e57b8.firebasestorage.app",
+  messagingSenderId: "420798143606",
+  appId: "1:420798143606:web:5c61b76b311161f4415357"
 };
 
 const app = initializeApp(firebaseConfig);
